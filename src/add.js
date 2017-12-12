518b56d59b46fe1013d6ddd389c087f4b0f4dc2c
Van Pho Nguyen <vanpho02@gmail.com>
2017-12-12 18:01:20 +0700

    add callback

--- src/add.js
+++ src/add.js
@@ -2,4 +2,11 @@ function add(a, b) {
     return a + b;
 }
 
-module.exports = { add };
+function addCallback(a, b, cb) {
+    setTimeout(() => {
+        if (isNaN(a) || isNaN(b)) cb(new Error('Type error'));
+        cb(null, a + b);
+    }, 500)
+}
+
+module.exports = { add, addCallback };
--- test/addTest.js
+++ test/addTest.js
@@ -1,9 +1,14 @@
 const assert = require('assert');
-const { add } = require('../src/add');
+const { add, addCallback } = require('../src/add');
 
 it('Can add 2 numbers', () => {
     const tong = add(4, 5);
     // if (tong !== 9) throw new Error('loi');
     // assert(10 === tong);
     assert.equal(tong, 9);
+});
+
+it('Can add 2 negative numbers', () => {
+    const tong = add(-4, -10);
+    assert.equal(tong, -14);
 });
