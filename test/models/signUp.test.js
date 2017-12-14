2f32e170ed98112c208486d227d4d5ca46336368
Van Pho Nguyen <vanpho02@gmail.com>
2017-12-14 17:47:14 +0700

    sign up test

--- test/models/signUp.test.js
+++ test/models/signUp.test.js
@@ -1,4 +1,5 @@
 const assert = require('assert');
+const { compare } = require('bcrypt');
 const User = require('../../src/models/User');
 
 describe.only('Test sign up', () => {
@@ -6,5 +7,12 @@ describe.only('Test sign up', () => {
         await User.signUp('a@gmail.com', '123', 'abcd', '018231');
         const user = await User.findOne({ email: 'a@gmail.com' });
         assert.equal(user.name, 'abcd');
+        const same = await compare('123', user.password);
+        assert.equal(same, true);
+    });
+
+    it('Cannot sign up with duplicated email', async () => {
+        await User.signUp('a@gmail.com', '123', 'abcd', '018231');
+        
     });
 });
