a1bcf0f084e9a0deaabd0e2ea93100197e47e926
Van Pho Nguyen <vanpho02@gmail.com>
2017-12-14 18:21:16 +0700

    req

--- test/models/signIn.test.js
+++ test/models/signIn.test.js
@@ -2,20 +2,30 @@ const assert = require('assert');
 const { compare } = require('bcrypt');
 const User = require('../../src/models/User');
 
-describe.only('Test sign in', () => {
+describe('Test sign in', () => {
     beforeEach('Create new user for test', async () => {
-        await User.signUp('a@gmail.com', 'Pho', '08123781');
+        await User.signUp('a@gmail.com', '123', 'Pho', '08123781');
     });
 
     it('Can sign in with right info', async() => {
-
+        await User.signIn('a@gmail.com', '123');
     });
 
     it('Cannot sign in with wrong email', async() => {
-        
+        try {
+            await User.signIn('a1@gmail.com', '123');
+            throw new Error('Test fail');
+        } catch (err) {
+            assert.equal(err.message, 'User khong ton tai');
+        }
     });
 
     it('Can sign in with wrong email password', async() => {
-        
+        try {
+            await User.signIn('a@gmail.com', '1234');
+            throw new Error('Test fail');
+        } catch (err) {
+            assert.equal(err.message, 'Mat khau khong hop le');
+        }
     });
 });
