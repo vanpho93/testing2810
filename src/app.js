4554c4421d593bacf0a1df3a5b934116b5099cb9
Van Pho Nguyen <vanpho02@gmail.com>
2017-12-14 18:46:59 +0700

    add route

--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
 const express = require('express');
-
+const parser = require('body-parser').urlencoded({ extended: false });
 const app = express();
+
 app.get('/', (req, res) => res.send('123'));
+
+app.post('/cong', parser, (req, res) => {
+    const { soA, soB } = req.body;
+    if (isNaN(soA) || isNaN(soB)) return res.status(400).send('Fail');
+    res.send(+soA + +soB + '');
+});
 
 module.exports = { app };
