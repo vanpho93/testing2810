3cd955f27b849fd7f94d991c83e58c26d4077d86
Van Pho Nguyen <vanpho02@gmail.com>
2017-12-16 17:45:25 +0700

    Story

---
+++ src/models/Story.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const storySchema = new mongoose.Schema({
+    author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    title: { type: String, trim: true, require: true },
+    content: { type: String, trim: true, require: true }
+});
+
+const StoryModel = mongoose.model('Story', storySchema);
+
+class Story extends StoryModel {}
+
+model.exports = Story;
